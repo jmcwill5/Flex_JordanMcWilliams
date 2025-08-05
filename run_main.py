1e3dfde2031366ee068d1d31af86d434c63be263
jmcwill5 <jmcwill@iastate.edu>
2025-08-04 23:26:19 -0500

    Updated in code documentation / doc strings

--- run_main.py
+++ run_main.py
@@ -1,25 +1,34 @@
+'''run_main.py
+
+This script sets up a Flask web application that allows users to search for exercises based on muscle groups and difficulty levels.
+It also enables the creation of a personalized exercise plan by adding and removing exercises.'''
+
+
+# Necessary imports
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
-import os
+# Set working directory
 os.chdir(os.path.dirname(os.path.abspath(__file__))) # change working directory to the directory of this script
 print("Current working directory:", os.getcwd())
 
-# load dataframe once globally
+# Load dataframe once globally
 dfg = pd.read_csv('data/exercises.csv', encoding='latin1')
 dfg['primaryMuscles'] = dfg['primaryMuscles'].str.lower()
 dfg['level'] = dfg['level'].str.lower()
 
 @app.route('/')
-# route to home page
 def home():
+    '''Route to home page'''
     return render_template('home.html')
 
 @app.route('/search')
-# search function to handle requests for specific muscle types and difficulty levels
 def search():
+    '''Search function to handle requests for specific muscle types and difficulty levels.
+    Returns a results page with a filtered list of exercises based on user input.'''
     muscle = request.args.get('muscle', '').lower()
     level = request.args.get('level', '').lower()
 
@@ -44,8 +53,8 @@ def search():
 plan = [] # will store temporary exercise plan
 
 @app.route('/add_to_plan', methods=['POST'])
-# add exercises to "my exercise plan"
 def add_to_plan():
+    '''Adds exercises to the user's exercise plan if not already present'''
     exercise = {
         'name': request.form['name'],
         'muscle': request.form['muscle'],
@@ -58,13 +67,13 @@ def add_to_plan():
     return redirect(request.referrer)
 
 @app.route('/my_exercise_plan')
-# view "my exercise plan" page
 def view_plan():
+    '''Routes to the user's exercise plan page, displaying all exercises added by the user.'''
     return render_template('my_exercise_plan.html', plan=plan)
 
 @app.route('/delete_from_plan', methods=['POST'])
-# option to delete exercises from "my exercise plan"
 def delete_from_plan():
+    '''Deletes an exercise from the user's exercise plan based on user input.'''
     exercise_delete = {
         'name': request.form['name'],
         'muscle': request.form['muscle'],
